c26c217af1a04e37eae1e0ff73e404f60207c24d
Abdallah Altokhy <abdallahaltokhy@gmail.com>
2021-04-08 18:31:16 +0200

    Update server port

--- server/app.js
+++ server/app.js
@@ -8,5 +8,5 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`App listening on port ${port} 🚀🚀`));
